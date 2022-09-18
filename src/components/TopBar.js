dfd7eca75c100e91f99abb301f2811fc9ecc7332
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-09-18 19:33:53 +0530

    Add files via upload

---
+++ src/components/TopBar.js
@@ -0,0 +1,9 @@
+const Topbar=()=>{
+    return(
+        <div className="top-bar">
+            <img src="../images/dp.png"/>
+            
+        </div>
+        )
+}
+export default Topbar;
---
+++ src/components/TransactionTable.js
@@ -0,0 +1,22 @@
+const TransactionTable=(props)=>{
+    return(
+        <div className="table-container">
+            <h3 className="table-title">Active Deposits</h3>
+            <span className="transaction-no">2</span>
+            <button type="button" className="add-btn">+</button>
+
+            <table>
+                <tr>
+                    <th>participents</th>
+                    <th>MOVE IN DATE</th>
+                    <th>RENT</th>
+                    <th>DEPOSIT</th>
+                    <th>STATUS
+                        <span className="status-color"></span>
+                    </th>
+                </tr>
+            </table>
+        </div>
+    )
+}
+export default TransactionTable;
