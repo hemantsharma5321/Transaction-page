f9688f9017d18875343c73167caa7c0de71c406a
hemantsharma5321 <60885551+hemantsharma5321@users.noreply.github.com>
2022-09-19 08:21:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import './App.css';
+import './App.css'; 
 import SideNav from './components/SisdeNav';
 import TopBar from './components/TopBar';
 import TransactionTable from './components/TransactionTable';
